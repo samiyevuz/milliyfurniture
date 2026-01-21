b56e200719331fcbcea4894fdeb4c703c40dc0bd
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-21 17:49:20 +0500

    fbgd

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,15 +5,7 @@
     <title>Milliy Furniture</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
-
-
-    {{-- Tailwind / Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <link rel="stylesheet" href="{{ asset('assets/css/hero.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
-    
 </head>
 <body class="bg-[#F5F7FB] text-gray-800">
 
