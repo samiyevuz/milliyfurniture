a3019a49d5f273b3774f455fd0a3720b3a3b8dde
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-21 17:34:56 +0500

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <title>Milliy Furniture</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('assets/css/hero.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+
 
 
 
     {{-- Tailwind / Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <link rel="stylesheet" href="{{ asset('assets/css/hero.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+    
 </head>
 <body class="bg-[#F5F7FB] text-gray-800">
 
