b26d8c887c11c0e2ca917dfb430036f82c840b60
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-21 17:28:34 +0500

    xc

--- resources/views/frontend/sections/hero.blade.php
+++ resources/views/frontend/sections/hero.blade.php
@@ -38,7 +38,7 @@
                 <img src="{{ asset('assets/icons/delivery.svg') }}" alt="">
             </div>
             <div class="feature-text">
-                <h4>Freeeeeeeeeeeeeeeee Delivery</h4>
+                <h4>Free Delivery</h4>
                 <p>Lorem ipsum dolor sit amet.</p>
             </div>
         </div>
