55ae6eb72c64143edc80997840c0d67000ae37aa
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-21 15:57:14 +0500

    Update hero.blade.php

--- resources/views/frontend/sections/hero.blade.php
+++ resources/views/frontend/sections/hero.blade.php
@@ -4,7 +4,7 @@
         height: 100vh;
         background-image: url('{{ asset('assets/images/hero-bg.png') }}');
         background-size: cover;
-        background-position: center 52%;
+        background-position: center 100%;
         background-repeat: no-repeat;
     "
 >
