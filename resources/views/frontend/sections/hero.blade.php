bdf64593db26fddd30611a3ba9c761f7fe366ab2
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-22 15:30:49 +0500

    gdg

--- resources/views/frontend/sections/hero.blade.php
+++ resources/views/frontend/sections/hero.blade.php
@@ -168,30 +168,27 @@
 
         <div class="how-grid">
 
-            {{-- CARD 1 --}}
             <div class="how-card">
                 <div class="how-image">
-                    <img src="{{ asset('assets/images/how-1.png') }}" alt="Purchase Securely">
+                    <img src="{{ asset('assets/images/how-1.png') }}" alt="">
                     <span class="how-step">1</span>
                 </div>
                 <h4>Purchase Securely</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
-            {{-- CARD 2 --}}
             <div class="how-card">
                 <div class="how-image">
-                    <img src="{{ asset('assets/images/how-2.png') }}" alt="Ships From Warehouse">
+                    <img src="{{ asset('assets/images/how-2.png') }}" alt="">
                     <span class="how-step">2</span>
                 </div>
                 <h4>Ships From Warehouse</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
-            {{-- CARD 3 --}}
             <div class="how-card">
                 <div class="how-image">
-                    <img src="{{ asset('assets/images/how-3.png') }}" alt="Style Your Room">
+                    <img src="{{ asset('assets/images/how-3.png') }}" alt="">
                     <span class="how-step">3</span>
                 </div>
                 <h4>Style Your Room</h4>
@@ -202,4 +199,3 @@
 
     </div>
 </section>
-
