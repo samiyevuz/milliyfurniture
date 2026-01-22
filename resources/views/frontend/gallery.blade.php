1803d9ab9fbea4914f61d14c620831bfdc14e452
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-22 17:43:09 +0500

    Update gallery.blade.php

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -1,18 +1,44 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-<section class="gallery-page">
-    <div class="gallery-container">
-        <h1>Gallery</h1>
 
-        <div class="gallery-grid">
-            <img src="{{ asset('assets/images/gallery-1.png') }}">
-            <img src="{{ asset('assets/images/gallery-2.png') }}">
-            <img src="{{ asset('assets/images/gallery-3.png') }}">
-            <img src="{{ asset('assets/images/gallery-4.png') }}">
-            <img src="{{ asset('assets/imagesgallery-5.png') }}">
-            <img src="{{ asset('assets/images/gallery-6.png') }}">
-        </div>
+{{-- PAGE HEADER --}}
+<section class="gallery-hero">
+    <div class="gallery-hero-container">
+        <h1>Gallery</h1>
+        <p>
+            Explore our handcrafted furniture collections
+            designed to elevate your living space.
+        </p>
     </div>
 </section>
+
+{{-- GALLERY GRID --}}
+<section class="gallery-page">
+    <div class="gallery-container">
+
+        <div class="gallery-grid">
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-1.png') }}" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-2.png') }}" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-3.png') }}" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-4.png') }}" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-5.png') }}" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="{{ asset('assets/images/gallery-6.png') }}" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
+
 @endsection
