c8e4e2a134bf395de24a6475418a89ecf8599e60
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-24 17:39:44 +0500

    Update app.blade.php

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -33,7 +33,9 @@
     Testimonials
 </a>
 
-
+            <a href="#" class="block px-4 py-2 rounded-lg hover:bg-[#F5F7FB]">
+                Gallery
+            </a>
             <a href="#" class="block px-4 py-2 rounded-lg hover:bg-[#F5F7FB]">
                 Settings
             </a>
