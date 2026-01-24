88fadf6494cb7b4c4e4c3763d27cf91555c15a46
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-24 20:34:42 +0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.time-input').forEach(input => {
+
+        input.addEventListener('input', e => {
+            let v = e.target.value.replace(/\D/g, '');
+
+            if (v.length >= 3) {
+                v = v.slice(0, 2) + ':' + v.slice(2, 4);
+            }
+
+            e.target.value = v.slice(0, 5);
+        });
+
+        input.addEventListener('blur', e => {
+            const val = e.target.value;
+
+            // faqat 00:00 – 23:59
+            if (val !== '' && !/^([01]\d|2[0-3]):([0-5]\d)$/.test(val)) {
+                e.target.value = '';
+            }
+        });
+
+    });
+});
