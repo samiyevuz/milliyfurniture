9d3b38889fcd0aeba600cf09988ecb1c9a180a87
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-24 15:15:02 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryControllerV2;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\TestimonialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +41,26 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('products', ProductController::class)
         ->except(['show']);
 });
+
+/*
+|--------------------------------------------------------------------------
+| Testimonials CRUD
+|--------------------------------------------------------------------------
+*/
+Route::get('/testimonials', [TestimonialController::class, 'index'])
+    ->name('testimonials.index');
+
+Route::get('/testimonials/create', [TestimonialController::class, 'create'])
+    ->name('testimonials.create');
+
+Route::post('/testimonials', [TestimonialController::class, 'store'])
+    ->name('testimonials.store');
+
+Route::get('/testimonials/{testimonial}/edit', [TestimonialController::class, 'edit'])
+    ->name('testimonials.edit');
+
+Route::put('/testimonials/{testimonial}', [TestimonialController::class, 'update'])
+    ->name('testimonials.update');
+
+Route::delete('/testimonials/{testimonial}', [TestimonialController::class, 'destroy'])
+    ->name('testimonials.destroy');
