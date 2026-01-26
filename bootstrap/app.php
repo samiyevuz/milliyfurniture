383bdc6b9a80540ea1eb8f70580b4f0040330010
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-01-26 09:19:20 +0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectUsersTo(fn () => route('admin.login'));
+        $middleware->redirectGuestsTo(fn () => route('admin.login'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
